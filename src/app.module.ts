df8267bc0fa35563f2d9dffd237b017557665db8
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-04-10 11:00:00 +0600

    validation pipe added  in main.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { ChatModule } from './chat/chat.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [BlogModule, ChatModule],
+  imports: [
+    BlogModule,
+    ChatModule,
+    CatsModule,
+    // MongooseModule.forRoot('mongodb://localhost:27017/nestMongo'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
